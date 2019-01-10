9270f393857e590cd653bc51125ae74e221ecd9d
Alua Baikadamova <abaikad00@gmail.com>
2019-01-10 03:40:52 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -8,7 +8,13 @@ class Anagram
   end
   
   def match (array)
-    
+    array.each do |arr|
+      
+      if arr.split("")==@word.split("")
+        result=arr
+      end
+    end
+    return result
     
   end
 end 
